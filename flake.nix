51c613ce803c7c0f9711d37a337f071f75b5be83
hugosenari <hugosenari@gmail.com>
2022-03-12 02:52:24 -0300

    chore: re index lazy

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Iterator library for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."lazy-master".url = "path:./master";
+  inputs."lazy-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."lazy-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
